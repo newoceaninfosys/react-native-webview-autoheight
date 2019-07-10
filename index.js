a721af8bd856a49ed14adbe19e1a76593a63c85f
sangdao <sang.dao@newoceaninfosys.com>
2019-07-10 17:49:02 +0700

    RN 0.60.0

--- index.js
+++ index.js
@@ -1,138 +1,148 @@
-import React, {Component} from 'react';
-import {
-  View,
-  Dimensions,
-} from 'react-native';
-import { WebView } from 'react-native-webview';
+import React, { Component } from "react";
+import { View, Dimensions } from "react-native";
+import { WebView } from "react-native-webview";
 
-const injectedScript = function () {
-    function waitForBridge() {
-        if (window.postMessage.length !== 1) {
-            setTimeout(waitForBridge, 200);
-        } else {
-            let height = 0;
-            if (document.documentElement.clientHeight > document.body.clientHeight) {
-                height = document.documentElement.clientHeight
-            } else {
-                height = document.body.clientHeight
-            }
-            postMessage({height})
-        }
+const injectedScript = function() {
+  function waitForBridge() {
+    if (window.postMessage.length !== 1) {
+      setTimeout(waitForBridge, 200);
+    } else {
+      let height = 0;
+      if (document.documentElement.clientHeight > document.body.clientHeight) {
+        height = document.documentElement.clientHeight;
+      } else {
+        height = document.body.clientHeight;
+      }
+      postMessage({ height });
     }
+  }
 
-    waitForBridge();
+  waitForBridge();
 };
 
-const patchPostMessageFunction = function () {
-    const originalPostMessage = window.postMessage;
-    const patchedPostMessage = function (message, targetOrigin, transfer) {
-        originalPostMessage(message, targetOrigin, transfer);
-    };
-    patchedPostMessage.toString = function () {
-        return String(Object.hasOwnProperty).replace('hasOwnProperty', 'postMessage');
-    };
-    window.postMessage = patchedPostMessage;
-    window.postMessageNew = patchedPostMessage;
+const patchPostMessageFunction = function() {
+  const originalPostMessage = window.postMessage;
+  const patchedPostMessage = function(message, targetOrigin, transfer) {
+    originalPostMessage(message, targetOrigin, transfer);
+  };
+  patchedPostMessage.toString = function() {
+    return String(Object.hasOwnProperty).replace(
+      "hasOwnProperty",
+      "postMessage"
+    );
+  };
+  window.postMessage = patchedPostMessage;
+  window.postMessageNew = patchedPostMessage;
 };
 
 export default class MyWebView extends Component {
-    state = {
-        webViewHeight: Number
+  state = {
+    webViewHeight: Number
+  };
+
+  static defaultProps = {
+    autoHeight: true
+  };
+
+  constructor(props: Object) {
+    super(props);
+    this.state = {
+      webViewHeight: this.props.defaultHeight
     };
 
-    static defaultProps = {
-        autoHeight: true,
+    this._onMessage = this._onMessage.bind(this);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return (
+      this.state.webViewHeight !== nextState.webViewHeight ||
+      (nextProps.source.uri && this.props.source.uri
+        ? this.props.source.uri.length !== nextProps.source.uri.length
+        : true) ||
+      (nextProps.source.html && this.props.source.html
+        ? this.props.source.html.length !== nextProps.source.html.length
+        : true)
+    );
+  }
+
+  decode = input => {
+    let output = input;
+    for (let i = 0; i < 5; i++) {
+      output = decodeURI(output);
     }
 
-    constructor(props: Object) {
-        super(props);
-        this.state = {
-            webViewHeight: this.props.defaultHeight
-        }
-
-        this._onMessage = this._onMessage.bind(this);
+    for (let i = 0; i < 5; i++) {
+      output = decodeURIComponent(output);
     }
 
-    shouldComponentUpdate(nextProps, nextState) {
-        return this.state.webViewHeight !== nextState.webViewHeight ||
-            (nextProps.source.uri && this.props.source.uri ? this.props.source.uri.length !== nextProps.source.uri.length : true) ||
-            (nextProps.source.html && this.props.source.html ? this.props.source.html.length !== nextProps.source.html.length : true)
+    return output;
+  };
+
+  _onMessage = e => {
+    const data = this.decode(e.nativeEvent.data);
+    if (/^{/.test(data)) {
+      this.setState({
+        webViewHeight: JSON.parse(data)["height"]
+      });
     }
+  };
 
-    decode = (input) => {
-        let output = input;
-        for (let i = 0; i < 5; i++) {
-            output = decodeURI(output);
-        }
+  onLoadEnd = () => {
+    const { additionalInjectedJavaScript } = this.props;
+    this.webview.injectJavaScript(`
+        try {
+            var count = 0;
+            var checkHeight = function () {
+                var height = 0;
+                if(document.documentElement.clientHeight > document.body.clientHeight)
+                {
+                    height = document.documentElement.clientHeight
+                }
+                else
+                {
+                    height = document.body.clientHeight
+                }
+                window.ReactNativeWebView.postMessage(JSON.stringify({height}));
 
-        for (let i = 0; i < 5; i++) {
-            output = decodeURIComponent(output);
-        }
+                count++;
 
-        return output;
-    }
+                if(count < 5) {
+                    setTimeout(function() {
+                        checkHeight();
+                    }, 800);
+                }
+            }
 
-    _onMessage = (e) => {
-        const data = this.decode(e.nativeEvent.data);
-        if (/^{/.test(data)) {
-            this.setState({
-                webViewHeight: JSON.parse(data)['height']
-            });
-        }
-    }
-
-    onLoadEnd = () => {
-        const {additionalInjectedJavaScript} = this.props;
-        this.webview.injectJavaScript(`
-      var count = 0;
-      var checkHeight = function () {
-        var height = 0;
-        if(document.documentElement.clientHeight > document.body.clientHeight)
-        {
-          height = document.documentElement.clientHeight
-        }
-        else
-        {
-          height = document.body.clientHeight
-        }
-        postMessage(JSON.stringify({height}));
-
-        count++;
-
-        if(count < 5) {
-          setTimeout(function() {
             checkHeight();
-          }, 800);
-        }
-      }
-
-      checkHeight();
-      
-      ${additionalInjectedJavaScript || ''}
+            
+            ${additionalInjectedJavaScript || ""} 
+        } catch (e) { window.ReactNativeWebView.postMessage(JSON.stringify(e.message)); }
     `);
-    }
+  };
 
-    stopLoading = () => {
-        this.webview.stopLoading();
-    }
+  stopLoading = () => {
+    this.webview.stopLoading();
+  };
 
-    render() {
-        const _w = this.props.width || Dimensions.get('window').width;
-        const _h = this.props.autoHeight ? this.state.webViewHeight : this.props.defaultHeight;
-        return (
-            <WebView
-                ref={(ref) => {
-                    this.webview = ref;
-                }}
-                injectedJavaScript={'(' + String(patchPostMessageFunction) + ')();'}
-                scrollEnabled={this.props.scrollEnabled || false}
-                onMessage={this._onMessage}
-                javaScriptEnabled={true}
-                onLoadEnd={this.onLoadEnd}
-                automaticallyAdjustContentInsets={true}
-                {...this.props}
-                style={[{width: _w}, this.props.style, {height: _h}]}
-            />
-        )
-    }
+  render() {
+    const _w = this.props.width || Dimensions.get("window").width;
+    const _h = this.props.autoHeight
+      ? this.state.webViewHeight
+      : this.props.defaultHeight;
+    return (
+      <WebView
+        ref={ref => {
+          this.webview = ref;
+        }}
+        injectedJavaScript={"const meta = document.createElement('meta'); meta.setAttribute('content', 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta);true"}
+        scrollEnabled={this.props.scrollEnabled || false}
+        onMessage={this._onMessage}
+        javaScriptEnabled={true}
+        onLoadEnd={this.onLoadEnd}
+        automaticallyAdjustContentInsets={true}
+        {...this.props}
+        style={[{ width: _w }, this.props.style, { height: _h }]}
+      />
+    );
+  }
 }
